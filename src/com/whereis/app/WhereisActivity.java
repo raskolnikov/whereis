dee4d37ddf27de7bea68428a600341f0accae340
Raskolnikov <mehmet2aktas@gmail.com>
2011-12-01 14:03:44 -0800

    first commit

---
+++ src/com/whereis/app/WhereisActivity.java
@@ -0,0 +1,13 @@
+package com.whereis.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WhereisActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
